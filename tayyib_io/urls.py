e6d214f08bcac1d41f026a974e1140ca70c34a6c
zaydiy <iamzaydiy@gmail.com>
2026-04-26 21:17:09 +0500

    feat: security changes

--- tayyib_io/urls.py
+++ tayyib_io/urls.py
@@ -4,6 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secure-tayyib-panel/', admin.site.urls),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
